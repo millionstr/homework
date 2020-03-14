339d8c949672ab055c1f0a5edd36f4cb2cf8162e
Ruslan <rsafonov@ukr.net>
2020-03-14 21:44:52 +0200

    over game

--- ex2_1.py
+++ ex2_1.py
@@ -2,4 +2,7 @@
 необхідно створити програму для випадкового вибору переможця зі списку.
 список гравців має бути заданий у коді.
 """
+import random
 lists=["Toma","Luda","Vlad","Rostislav","Anastasija","Ruslan"]
+print(lists)
+print("Переможець: ",random.choice(lists))
