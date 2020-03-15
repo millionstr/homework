3e5f87bd7a34e01e5daf099016f1767bd6780d7e
Ruslan <rsafonov@ukr.net>
2020-03-15 21:17:54 +0200

    day week maxmonth end

--- ex2_3.py
+++ ex2_3.py
@@ -3,11 +3,17 @@
 та зазначити чи день є вихідним
 """
 import calendar
-data = "12-07-2019"
+data = "23-02-2020"
 year = int(data[6:])
 month = int(data[3:5])
 day = int(data[:2])
-print(year, month,day)
-print(calendar.weekday(year,month,day))
-dayweek, max_day_month = calendar.monthrange(year,month)
-print(max_day_month)
+#день тижня від 0 до 6, 6-неділя (day_week)
+day_week = calendar.weekday(year,month,day)
+# fist_day_week - перший день місяця(0-Пн .. 6-Нд), кількість днів в місяці - max_day_month
+fist_day_week, max_day_month = calendar.monthrange(year,month)
+
+Month = ["","січні","лютому","березні","квітні","травні","червні","липні",\
+    "серпні","вересні","жовтні","листопаді","грудні"]
+print(f"У {Month[month]} {year} року {max_day_month} день")
+if day_week == 6:
+    print(f"{data[:2]}.{data[3:5]}.{data[6:]} - вихідний")
