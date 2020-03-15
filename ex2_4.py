4586f76a8517cdd55011df3fc3b82608f9291894
Ruslan <rsafonov@ukr.net>
2020-03-15 21:45:28 +0200

    end

--- ex2_4.py
+++ ex2_4.py
@@ -11,3 +11,10 @@ for i in range(10):
     lists.append(random.randint(-10,-1))
     lists.append(random.randint(1,10))
 print(lists, len(lists))
+
+namber = int(input("введіть ціле число: "))
+repeat_namber=0
+for i in lists:
+    if i == namber:
+        repeat_namber += 1
+print(f"число {namber} повторюється в масиві {repeat_namber} разів")
